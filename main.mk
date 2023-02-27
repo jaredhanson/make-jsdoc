6d3ab8d22380f813431383df460895d13dde6c5c
Jared Hanson <jaredhanson@gmail.com>
2023-02-26 16:20:07 -0800

    Add help.

--- main.mk
+++ main.mk
@@ -4,8 +4,9 @@ prefix ?= .
 docdir ?= $(prefix)/doc
 htmldir ?= $(docdir)
 
+
+html: $(call print-help,html,Generate documentation files in HTML format) $(htmldir)/index.html
 .PHONY: html
-html: $(htmldir)/index.html
 
 $(htmldir)/index.html: $(SOURCES)
 	$(JSDOC) $(JSDOCFLAGS) -d $(htmldir) $^
